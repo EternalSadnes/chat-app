910a45db6b4e9b33ebca3e2582c872d1da8ccf06
Dmytro Lysko <dimonishe322@gmail.com>
2022-12-04 14:55:41 +0200

    remove PreAuthorize

--- src/main/java/com/eternal/chatapp/controller/HelloController.java
+++ src/main/java/com/eternal/chatapp/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.eternal.chatapp.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    @PreAuthorize("hasRole('USER')")
     public String hello() {
         return "Hello!";
     }
